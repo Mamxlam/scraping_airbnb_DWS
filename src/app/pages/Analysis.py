39725f25e724864a82bd0d562485dac1625fbe63
mamxlam <mamxlam@teiemt.gr>
2024-04-20 14:56:04 +0300

    Added various changes in analysis page of Streamlit App (Map, dynamic page adjustment, filtering per cluster, correlation matrix per property type, map drafts).

--- src/app/pages/Analysis.py
+++ src/app/pages/Analysis.py
@@ -15,15 +15,23 @@ from os import getcwd
 import os
 path.append(os.path.join(getcwd(),"..","..","src"))
 from pyscripts.Preprocessor import Preprocessor
+import pydeck as pdk
+import folium 
+from streamlit_folium import folium_static
 
 st.set_page_config(layout="wide")
+
+# Calculate figure width and height dynamically based on window width
+WIN_WIDTH = st.sidebar.number_input('Enter window width', value=800)
+FIG_WIDTH = 0.9 * WIN_WIDTH  # 90% of window width or maximum of 800 pixels
+FIG_HEIGHT = 0.4 * FIG_WIDTH  # Maintain aspect ratio
 
 # *** Data Loading ***
 @st.cache_data  # Caching to improve load times
 def load_airbnb_data():
     return pd.read_csv('../../data/listing_data_postproc_TOKEEP.csv', index_col=0)
 
-def corr_matrix(df):
+def corr_matrix(df, title="Correlation Matrix"):
     # 1. Select the relevant columns
     corr_data = df.select_dtypes(exclude=['object'])
 
@@ -44,9 +52,15 @@ def corr_matrix(df):
     fig = px.imshow(corr_matrix,
                     text_auto=True,  # Display correlation values in the cells
                     aspect="auto",  # Adjust aspect ratio for readability
-                    title="Correlation Matrix", 
+                    title=title, 
                     color_continuous_scale=reversed_color_scale  # Apply the reversed colormap
                 )
+    
+    # Update layout to adjust figure size
+    fig.update_layout(
+        width=FIG_WIDTH,  # Set width of the figure
+        height=FIG_HEIGHT  # Set height of the figure
+    )
     st.plotly_chart(fig) 
 
 def clustering(df):
@@ -82,8 +96,14 @@ def clustering(df):
 
     # Cluster visualization
     fig = px.scatter_3d(df, x=columns[0], y=columns[1], z=columns[2], color='cluster', title='Cluster Visualization')
+    fig.update_layout(
+        width=FIG_WIDTH,  # Set width of the figure
+        height=FIG_HEIGHT  # Set height of the figure
+    )
     # Display the plot in Streamlit
     st.plotly_chart(fig) 
+
+    return df['cluster']
 
 
 
@@ -114,7 +134,10 @@ def bot_top_rated_stays(df):
                 color_continuous_scale='YlGn', orientation='v',
                 title='Top Rated Stays')
 
-    fig.update_layout(xaxis={'categoryorder': 'total descending'})  # Ensure correct order 
+    fig.update_layout(xaxis={'categoryorder': 'total descending'},# Ensure correct order 
+                      width=FIG_WIDTH,  # Set width of the figure
+                      height=FIG_HEIGHT  # Set height of the figure
+                    )  
 
     st.plotly_chart(fig) 
 
@@ -123,7 +146,10 @@ def bot_top_rated_stays(df):
                 color_continuous_scale='YlOrRd_r', orientation='v',
                 title='Bottom Rated Stays')
 
-    fig.update_layout(xaxis={'categoryorder': 'total descending'})  # Ensure correct order 
+    fig.update_layout(xaxis={'categoryorder': 'total descending'},# Ensure correct order 
+                      width=FIG_WIDTH,  # Set width of the figure
+                      height=FIG_HEIGHT  # Set height of the figure
+                      )  
 
     st.plotly_chart(fig) 
 
@@ -147,32 +173,282 @@ if nanExists:
     # Perform prediction on those columns to impute the NaN values
     df = processor.impute_predictor(df,miscols)
 
-# Select box to choose columns
-selected_columns = st.multiselect('Select columns to display:', df.columns)
 
-
-# col1, col2= st.columns([0.7,0.3])
-
-# with col1:
-# Filter DataFrame based on selected columns
-if selected_columns:
-    st.write('Filtered DataFrame:')
-    st.write(df[selected_columns])
-    corr_matrix(df[selected_columns])
-else:
-    selected_columns = ['Price', 'Visitors', 'Beds', 'Bedrooms']
-    st.write(df)
-    corr_matrix(df[selected_columns])
-
-st.markdown("---")
+st.write('##### Assign cluster number to derive type of properties.')
 
 # Select box to choose columns
 features = st.multiselect('Select at least 3 columns for clustering (first 3 will be shown in plot):', df.columns)
 if len(features) >= 3:
-    clustering(df[features])
+    clusters = clustering(df[features])
 else:
     features = ['Price', 'Visitors', 'Beds', 'Bedrooms']
-    clustering(df[features])
+    clusters = clustering(df[features])
 
-# with col2:
+
+
+st.markdown("---")
+
+st.write('##### According to cluster types and desired data, plot your own correlation matrices.')
+
+df['clusters'] = clusters
+
+# Select box to choose columns
+selected_columns = st.multiselect('Select columns to plot correlation matrix:', df.columns)
+
+# Create a multiselect widget on the sidebar for selecting municipalities
+selected_clusters = st.multiselect(
+    'Cluster type of properties to consider in correlation matrix (All clusters when none selected): ',
+    df['clusters'].unique()  # List of unique regions from the DataFrame
+)
+
+if not selected_clusters:
+    selected_clusters = df['clusters'].unique()
+
+if selected_columns:
+    st.write('Filtered DataFrame:')
+    st.write(df[selected_columns])
+    corr_matrix(df[selected_columns].loc[(data['clusters'].isin(selected_clusters))])
+else:
+    selected_columns = ['Price', 'Visitors', 'Beds', 'Bedrooms']
+    st.write(df)
+    corr_matrix(df[selected_columns].loc[(data['clusters'].isin(selected_clusters))])
+
+
+st.markdown("---")
+
+st.write('##### Review Top and Worst Rated Listings with penalization of low number of reviews.')
+
 bot_top_rated_stays(df)
+
+st.markdown("---")
+
+
+st.write('##### Review most common characteristics of all available listings.')
+
+# Calculate the counts for each characteristic
+char_counts = df.filter(regex='char_*').sum().sort_values(ascending=False)
+
+# Define color scale for countplot
+color_scale = px.colors.diverging.RdYlGn[::-1]  # Reverse the color scale
+
+
+# Plot the countplot
+fig1 = px.bar(
+    x=char_counts.index,
+    y=char_counts.values,
+    color=char_counts.values,
+    color_continuous_scale=color_scale,
+    labels={'x': 'Characteristic', 'y': 'Count'},
+    title='Countplot of Characteristics',
+    color_continuous_midpoint=char_counts.values.mean()  # midpoint for the color scale
+)
+
+fig1.update_layout(
+        width=FIG_WIDTH,  # Set width of the figure
+        height=FIG_HEIGHT  # Set height of the figure
+    )
+
+st.plotly_chart(fig1)
+
+
+
+corr_matrix(df.filter(regex=r'(char_*|Review)'), title="Corellation of Characteristics and Ratings")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ============================================================================================================
+# MAP EXPERIMENTATION
+
+
+
+# # Create a PyDeck scatter plot
+# view = pdk.ViewState(latitude=data['Latitude'].mean(),
+#                     longitude=data['Longitude'].mean(),
+#                     zoom=11,
+#                     pitch=50)
+
+# scatter_layer = pdk.Layer(
+#     'HexagonLayer',
+#     data=data,
+#     get_position='[Longitude, Latitude]',
+#     radius=40,
+#     elevation_scale=4,
+#     elevation_range=[0, 1000],
+#     pickable=True,
+#     extruded=True
+# )
+
+# tooltip = {
+#     "html": "<b>Name:</b>{elevationValue}<br/><b>Info:</b> {Info}",
+#     "style": {
+#         "backgroundColor": "steelblue",
+#         "color": "white"
+#     }
+# }
+
+# scatter_plot = pdk.Deck(
+#     layers=[scatter_layer],
+#     initial_view_state=view,
+#     tooltip=tooltip
+# )
+
+# # Show the PyDeck plot
+# st.pydeck_chart(scatter_plot)
+
+# # Add an interactive widget to capture the selected data point
+# selected_point_index = st.session_state.get('selected_point_index', None)
+
+# print(selected_point_index)
+
+# if selected_point_index is not None:
+#     st.write("Selected Point Info:")
+#     st.write(data[selected_point_index])
+
+# # Capture the selected data point using the 'on_click' event
+# if scatter_plot.deck_widget:
+#     scatter_plot.deck_widget.on_click(
+#         lambda x, y, feature_index: st.session_state.update(selected_point_index=feature_index)
+# )
+
+
+# Create a PyDeck scatter plot
+view = pdk.ViewState(latitude=data['Latitude'].mean(),
+                    longitude=data['Longitude'].mean(),
+                    zoom=11,
+                    pitch=50)
+
+scatter_layer = pdk.Layer(
+    'HexagonLayer',
+    data=data,
+    get_position='[Longitude, Latitude]',
+    get_elevation='Price',
+    radius=40,
+    elevation_scale=4,
+    elevation_range=[0, 1000],
+    pickable=True,
+    extruded=True
+)
+
+tooltip = {
+    "html": "<b>{Longitude}</b> meters away from an MRT station, costs <b>{Latitude}</b> NTD/sqm",
+    "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
+}
+
+scatter_plot = pdk.Deck(
+    layers=scatter_layer,
+    initial_view_state=view,
+    tooltip=tooltip
+)
+
+# Show the PyDeck plot
+st.pydeck_chart(scatter_plot)
+
+# def plot_map(data):
+#     # Center the map on a reasonable starting location 
+#     center_lat = data['Latitude'].mean()
+#     center_lon = data['Longitude'].mean()
+
+#     # Create the Folium map object
+#     map = folium.Map(location=[center_lat, center_lon], zoom_start=12)
+
+#     # Columns you want to display in the popup
+#     display_columns = [
+#         'Price', 'Title', 'Visitors', 'Bedrooms', 'Guest Favorite', 'Superhost', 
+#         'Number of Reviews'
+#     ]
+
+#     # Iterate through your data to add markers with popups
+#     for _, row in data.iterrows():
+#         html = f""""""
+
+#         # Add more information based on the display_columns list 
+#         for col in display_columns:
+#             if col in data.columns:
+#                 html += f"<b>{col}:</b> {row[col]}<br>"
+
+#         iframe = folium.IFrame(html=html, width=250, height=150)
+#         popup = folium.Popup(iframe, max_width=250)
+
+#         folium.Marker(
+#             location=[row['Latitude'], row['Longitude']],
+#             popup=popup,
+#             icon=folium.Icon(color='red', icon='info-sign')  # Customize color if needed
+#         ).add_to(map)
+
+
+#     # Streamlit display
+#     st.title("Interactive Property Map")
+#     folium_static(map)
+
+# plot_map(data)
