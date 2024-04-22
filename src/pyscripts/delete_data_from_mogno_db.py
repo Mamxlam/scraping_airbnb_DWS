397c49d05ed1db48cad6f3d6175c4554aced9034
Νικόλαος Μπουζιανάς <nickbouzi@iti.gr>
2024-04-22 20:00:43 +0300

    [Change]: Mongo code
    change the credentials

--- src/pyscripts/delete_data_from_mogno_db.py
+++ src/pyscripts/delete_data_from_mogno_db.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
 
 def mongo_connect():
-    HOST_NAME = 'db.csd.auth.gr'
-    PASSWORD = 'Gm8WQhwE'
-    USERNAME = 'eu'
-    AUTH_SOURCE = 'admin'
-    PORT = 27117
+    HOST_NAME = 'host'
+    PASSWORD = 'pass'
+    USERNAME = 'username'
+    AUTH_SOURCE = 'auth source'
+    PORT = 'port'
     database_name, collect_name = 'eu', 'teamA'
     connection_link = f"mongodb://{USERNAME}:{PASSWORD}@{HOST_NAME}:{PORT}/{AUTH_SOURCE}"
     my_client = MongoClient(connection_link)
